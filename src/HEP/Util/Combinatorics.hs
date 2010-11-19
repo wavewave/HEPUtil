8fae7e1cd60628e39df02baed2d70e5decb9f5fb
ianwookim <ianwookim@gmail.com>
2010-11-19 07:56:31 +0000

    add Combinatorics

--- src/HEP/Util/Combinatorics.hs
+++ src/HEP/Util/Combinatorics.hs
@@ -1,6 +1,6 @@
 module HEP.Util.Combinatorics where
 
-combinationsof :: Int -> [a] -> [[a]]
+combinationsOf :: Int -> [a] -> [[a]]
 combinationsOf 0 _ = [[]]
 combinationsOf _ [] = []
 combinationsOf k (x:xs) = map (x:) (combinationsOf (k-1) xs) ++ combinationsOf k xs
@@ -8,7 +8,7 @@ combinationsOf k (x:xs) = map (x:) (combinationsOf (k-1) xs) ++ combinationsOf k
 combinations :: Int -> Int -> [[Int]]
 combinations k n = combinationsOf k [1..n]
 
-comSep :: Int -> [a] -> ([[a]],[[a]])
+combSep :: (Eq a) => Int -> [a] -> [([a],[a])]
 combSep k lst = let cmblst = combinationsOf k lst 
                     f x = (x,filter (not.((flip elem) x)) lst)
                 in map f cmblst 
