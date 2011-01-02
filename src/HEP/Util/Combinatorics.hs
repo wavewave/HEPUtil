4971bd6a941d21013b7d6b502b6533bc35b507a7
ianwookim <ianwookim@gmail.com>
2011-01-02 23:31:59 +0000

    add shufflematrix

--- src/HEP/Util/Combinatorics.hs
+++ src/HEP/Util/Combinatorics.hs
@@ -1,5 +1,7 @@
 module HEP.Util.Combinatorics where
 
+import Numeric.LinearAlgebra
+
 combinationsOf :: Int -> [a] -> [[a]]
 combinationsOf 0 _ = [[]]
 combinationsOf _ [] = []
@@ -12,3 +14,12 @@ combSep :: (Eq a) => Int -> [a] -> [([a],[a])]
 combSep k lst = let cmblst = combinationsOf k lst 
                     f x = (x,filter (not.((flip elem) x)) lst)
                 in map f cmblst 
+
+permutations :: [a] -> [[a]] 
+permutations = undefined
+
+
+
+shufflematrix :: (Int,Int) -> ((Int->Int),(Int->Int)) -> Matrix Double -> Matrix Double
+shufflematrix (n1,n2) (perm1,perm2) m1 = 
+  (n1><n2) [ m1 @@> (perm1 i,perm2 j)  | i <- [0..(n1-1)], j <- [0..(n2-1)] ]  
