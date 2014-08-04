504ff9b860078d442eecb81a92afd03a3e9d6c35
Ian-Woo Kim <ianwookim@gmail.com>
2014-08-04 13:07:42 +0200

    revive HEP.Util.GHC.Plugins with the newest version of GHC api

--- src/HEP/Util/GHC/Plugins.hs
+++ src/HEP/Util/GHC/Plugins.hs
@@ -1,23 +1,18 @@
 module HEP.Util.GHC.Plugins where
 
 import Control.Monad.IO.Class
+import DynFlags
 import GHC 
 import GHC.Paths
-import DynFlags
 -- import Unsafe.Coerce
-
--- import Control.Monad
 import System.FilePath
-
--- import Data.List 
--- import Data.List.Split
-
+--
 import HEP.Util.File 
 
 
 pluginCompile :: FilePath -> String -> String -> IO (Either String HValue)
 pluginCompile basedir mname exp =  
-  defaultErrorHandler {- defaultDynFlags -} defaultLogAction $ do 
+  defaultErrorHandler defaultFatalMessager defaultFlushOut $ do 
     let (mdir,mfile) = moduleDirFile mname 
         fp = basedir </> mdir </> mfile
     f <- runGhc (Just libdir) $ do 
@@ -28,12 +23,10 @@ pluginCompile basedir mname exp =
       r <- load LoadAllTargets 
       case r of 
         Failed -> do 
-          -- putStrLn "Compilation Failed"
           return (Left "Compilation Failed") 
         Succeeded -> do 
-          -- putStrLn "Compilation Successed"
-          m <- findModule (mkModuleName mname) Nothing
-          setContext [IIModule m] -- ([] :: [InteractiveImport]) [(m,Nothing)]
+          -- m <- findModule (mkModuleName mname) Nothing
+          setContext [IIModule (mkModuleName mname)] 
           value <- compileExpr exp 
           return (Right value)          
     return f
