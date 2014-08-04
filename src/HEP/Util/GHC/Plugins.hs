bc16e245bf6e7d74ec18d59eff24db1d188234b0
Ian-Woo Kim <ianwookim@gmail.com>
2014-08-04 15:47:31 +0200

    pluginCompile now accept isDynamic flags to support ghci

--- src/HEP/Util/GHC/Plugins.hs
+++ src/HEP/Util/GHC/Plugins.hs
@@ -1,6 +1,7 @@
 module HEP.Util.GHC.Plugins where
 
 import Control.Monad.IO.Class
+import Data.Dynamic
 import DynFlags
 import GHC 
 import GHC.Paths
@@ -10,24 +11,37 @@ import System.FilePath
 import HEP.Util.File 
 
 
-pluginCompile :: FilePath -> String -> String -> IO (Either String HValue)
-pluginCompile basedir mname exp =  
+pluginCompile :: Bool -> FilePath -> String -> String -> IO (Either String HValue)
+pluginCompile isDynamic basedir mname exp =  
   defaultErrorHandler defaultFatalMessager defaultFlushOut $ do 
     let (mdir,mfile) = moduleDirFile mname 
         fp = basedir </> mdir </> mfile
     f <- runGhc (Just libdir) $ do 
+
       dflags <- getSessionDynFlags
-      setSessionDynFlags dflags
+      if isDynamic 
+        then setSessionDynFlags (dynamicTooMkDynamicDynFlags dflags)
+        else setSessionDynFlags dflags
       target <- guessTarget fp Nothing 
+
       addTarget target
+
       r <- load LoadAllTargets 
       case r of 
         Failed -> do 
           return (Left "Compilation Failed") 
         Succeeded -> do 
+    
+          liftIO $ putStrLn "stage1"
           -- m <- findModule (mkModuleName mname) Nothing
-          setContext [IIModule (mkModuleName mname)] 
+          setContext [IIDecl $ simpleImportDecl (mkModuleName mname)]
+             -- [IIModule (mkModuleName mname)] 
+          liftIO $ putStrLn "stage2"
+          liftIO $ print libdir
+
           value <- compileExpr exp 
+          -- dyn <- dynCompileExpr exp
+          -- let value = fromDyn dyn 
           return (Right value)          
     return f
 
